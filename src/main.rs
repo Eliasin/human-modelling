31ef5c3f2f830f2c6c57c3ff6fbcb690a73acf92
Steven Pham <steven.pham.8.2000@gmail.com>
2020-03-09 01:04:48 -0400

    Fill out building graph

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,8 @@ enum Building {
     FoodCourt,
     TimHortons,
     SecondCup,
-    Starbucks
+    Starbucks,
+    LibraryFirstFloor
 }
 
 impl Display for Building {
@@ -42,35 +43,45 @@ impl Display for Building {
             Building::TimHortons => write!(f, "{}", "TimHortons"),
             Building::SecondCup => write!(f, "{}", "SecondCup"),
             Building::Starbucks => write!(f, "{}", "Starbucks"),
+            Building::LibraryFirstFloor => write!(f, "{}", "LibraryFirstFloor"),
         }
     }
 }
 
 fn main() {
-    let mut buildings = Graph::<Building>::new(vec!(Building::LectureA, Building::LectureB, Building::LectureC, Building::LabA, Building::FoodCourt, Building::TimHortons, Building::SecondCup, Building::Starbucks));
+    let mut buildings = Graph::<Building>::new(vec!(Building::LectureA, Building::LectureB, Building::LectureC, Building::LabA, Building::FoodCourt, Building::TimHortons, Building::SecondCup, Building::Starbucks, Building::LibraryFirstFloor));
 
     buildings = buildings.add_undirected_edge(Building::LectureA, Building::FoodCourt, Some(5));
     buildings = buildings.add_undirected_edge(Building::LectureA, Building::LectureB, Some(5));
     buildings = buildings.add_undirected_edge(Building::LectureB, Building::LabA, Some(10));
     buildings = buildings.add_undirected_edge(Building::LectureB, Building::Starbucks, Some(10));
-    buildings = buildings.add_undirected_edge(Building::FoodCourt, Building::TimHortons, Some(3));
+    buildings = buildings.add_undirected_edge(Building::FoodCourt, Building::TimHortons, Some(5));
     buildings = buildings.add_undirected_edge(Building::SecondCup, Building::TimHortons, Some(5));
+    buildings = buildings.add_undirected_edge(Building::LibraryFirstFloor, Building::TimHortons, Some(5));
 
     let mut location_decider_hashmap = HashMap::new();
-    location_decider_hashmap.insert(Building::LectureA, (vec![Building::FoodCourt], vec![1]));
-    location_decider_hashmap.insert(Building::FoodCourt, (vec![Building::LectureA], vec![1]));
+    location_decider_hashmap.insert(Building::LectureA, (vec!(Building::FoodCourt, Building::LectureB, Building::TimHortons), vec!(1, 1, 1)));
+    location_decider_hashmap.insert(Building::FoodCourt, (vec!(Building::LectureA, Building::LectureB), vec!(1, 1)));
+    location_decider_hashmap.insert(Building::LectureB, (vec!(Building::LectureA, Building::Starbucks, Building::FoodCourt), vec!(1, 2, 1)));
+    location_decider_hashmap.insert(Building::TimHortons, (vec!(Building::LibraryFirstFloor), vec!(1)));
+    location_decider_hashmap.insert(Building::Starbucks, (vec!(Building::LibraryFirstFloor), vec!(1)));
+    location_decider_hashmap.insert(Building::LibraryFirstFloor, (vec!(Building::LectureA, Building::LectureB), vec!(1, 1)));
 
     let next_location_decider = MarkovChain::new(location_decider_hashmap);
     let mut when_to_leave_decider = HashMap::new();
 
     when_to_leave_decider.insert(Building::FoodCourt, 60);
     when_to_leave_decider.insert(Building::LectureA, 90);
+    when_to_leave_decider.insert(Building::LectureB, 90);
+    when_to_leave_decider.insert(Building::TimHortons, 30);
+    when_to_leave_decider.insert(Building::Starbucks, 15);
+    when_to_leave_decider.insert(Building::LibraryFirstFloor, 45);
 
-    let lecture_food_court_loop =
+    let typical_student =
         Schedule::new(next_location_decider, when_to_leave_decider);
     let entity = Entity::new(
         Stationary(Building::LectureA, 0),
-        lecture_food_court_loop,
+        typical_student,
     );
     
     let entities = vec!(entity.clone(); 100);
@@ -78,7 +89,7 @@ fn main() {
     let conjestion_relationships = vec!(lec_a_to_food_court);
     let mut entity_cluster = EntityCluster::new(entities, buildings, conjestion_relationships);
 
-    for _ in 0..110 {
+    for _ in 0..500 {
         println!("{}", entity_cluster.to_string());
         entity_cluster.apply_timestep(1);
     }
